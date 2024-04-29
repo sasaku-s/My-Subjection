12a8e3003d1f6e5aa67d5efa1734dad3c989ac36
sasaku-s <74994896+sasaku-s@users.noreply.github.com>
2024-04-29 20:26:35 +0800

    Update ISSM.m

--- ISSM.m
+++ ISSM.m
@@ -1,37 +1,47 @@
 clc
-clear
-close
-M=12;
-N=200;
-ts=0.01;
-f0=100;
-f1=80;
-f2=120;
-c=1500;
-lambda=c/f0;
-d=lambda/2;
-SNR=15;
+clear all
+close all
+%%%%%%%%%%%%%%%% 阵列模型 %%%%%%%%%%%%%
+M=12;                                     %阵元数
+N=300;                                    %快拍数
+ts=0.00125;                               %时域采样间隔
+f0=100;                                   %入射信号中心频率
+f1=80;                                    %入射信号最低频率
+f2=120;                                   %入射信号最高频率
+c=3e8;                                    %波速
+lambda=c/f0;                              %波长
+d=lambda/2;                               %阵元间距
+SNR=12;                                   %信噪比
 b=pi/180;
-theat1=30*b;%%%信号1
-theat2=25*b;%%%信号2
+theat1=0*b;                               %入射信号波束角1
+theat2=40*b;                              %入射信号波束角2
+theat3=45*b;
+theat4=-25*b;
 n=ts:ts:N*ts;
-theat=[theat1 theat2]';
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%信号处理%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-s1=chirp(n,80,1,120);%%线性调频1
-sa=fft(s1,2048);
-figure
-specgram(s1,256,1E3,256,250);%频谱
-s2=chirp(n+0.100,80,1,120);%%线性调频2
-sb=fft(s2,2048);
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%ISM%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-P=1:2;
-a=zeros(M,2);
+theat=[theat1 theat2 theat3 theat4]';
+
+%%%%%%%%%%%%%%%% 信号处理 %%%%%%%%%%%%%%%%
+s1=chirp(n,80,1,120);                      %线性调制
+sa=fft(s1,512);                           %FFT变换
+
+s2=chirp(n+0.125,80,1,120);                
+sb=fft(s2,512);                           
+
+s3=chirp(n+0.25,80,1,120);
+sc=fft(s3,512);
+
+s4=chirp(n+0.375,80,1,120);
+sd=fft(s4,512);
+
+%%%%%%%%%%%%%%%%%%%%% ISM算法 %%%%%%%%%%%%%%%%%%
+P=1:4;%%%信号数
+a=zeros(M,4);
 sump=zeros(1,181);
 for i=1:N
-    f=80+(i-1)*1.0;
-    s=[sa(i) sb(i)]';
+    f=80+(i-1)*1.25;
+    s=[sa(i) sb(i) sc(i) sd(i)]';%%接收信号矩阵
     for m=1:M
-        a(m,P)=exp(-1i*2*pi*d/c*sin(theat(P))*(m-1))';
+        a(m,P)=exp(-1i*2*pi*f*d/c*sin(theat(P))*(m-1))';
     end
     R=a*(s*s')*a';
     [em,zm]=eig(R);
@@ -53,10 +63,10 @@ for i=1:N
     end
     sump=sump+abs(p);
 end
-pmusic=1/33*sump;
+pmusic=(1/N)*sump;
 pm=1./pmusic;
 thetaesti=-90:1:90;
 plot(thetaesti,20*log(abs(pm)));
 xlabel('入射角/度');
-ylabel('空间谱/db');
+ylabel('空间谱/dB');
 grid on
